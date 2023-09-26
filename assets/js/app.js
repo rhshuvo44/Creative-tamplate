3d3dc48eae5a6134d4d0b53a8e9d841b81157b80
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-09-26 12:00:54 +0600

    portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// navbar
 const menuBTn = document.querySelector(".menu-icon span");
 const searchBTn = document.querySelector(".search-icon");
 const cancelBTn = document.querySelector(".cancel-icon");
@@ -22,3 +23,31 @@ searchBTn.onclick = () => {
   searchBTn.classList.add("hide");
   cancelBTn.classList.add("show");
 };
+
+// portfolio galler
+let list = document.querySelectorAll(".list");
+let itemBox = document.querySelectorAll(".itemBox");
+
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", function () {
+    for (let j = 0; j < list.length; j++) {
+      list[j].classList.remove("active");
+    }
+    this.classList.add("active");
+
+    let dataFilter = this.getAttribute("data-filter");
+
+    for (let k = 0; k < itemBox.length; k++) {
+      itemBox[k].classList.remove("active");
+      itemBox[k].classList.add("hide");
+
+      if (
+        itemBox[k].getAttribute("data-item") == dataFilter ||
+        dataFilter == "all"
+      ) {
+        itemBox[k].classList.remove("hide");
+        itemBox[k].classList.add("active");
+      }
+    }
+  });
+}
